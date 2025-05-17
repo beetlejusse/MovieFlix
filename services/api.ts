bb9cf357ad4b57e85934f09af85c34776eb02f7e
vishalBelwal <vishalbelwal52@gmail.com>
2025-05-17 21:02:33 +0530

    feat: Add functions to fetch movie details and trending movies

--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,4 @@
-export const TMDB_CONFIG= {
+export const TMDB_CONFIG = {
     BASE_URL: "https://api.themoviedb.org/3",
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     headers: {
@@ -7,7 +7,7 @@ export const TMDB_CONFIG= {
     }
 }
 // /discover/movie
-export const fetchMovies = async ({query}: {query: string}): Promise<Movie[]> => {
+export const fetchMovies = async ({ query }: { query: string }): Promise<Movie[]> => {
     const endPOint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endPOint, {
@@ -15,11 +15,29 @@ export const fetchMovies = async ({query}: {query: string}): Promise<Movie[]> =>
         headers: TMDB_CONFIG.headers
     })
 
-    if(!response.ok){
+    if (!response.ok) {
         //@ts-ignore
         throw new Error('Failed to fetch movies', `${response.statusText}`);
     }
 
     const data = await response.json();
     return data.results;
+}
+
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+    try {
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
+            method: 'GET',
+            headers: TMDB_CONFIG.headers
+        })
+
+        if (!response.ok) throw new Error(`Failed to fetch movie details: ${response.statusText}`)
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
 }
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -7,12 +7,12 @@ const client = new Client().setEndpoint("https://cloud.appwrite.io/v1").setProje
 const db = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
-    const result = await db.listDocuments(DATABASE_ID, COLLECTION_ID, [
-        Query.equal('searchTerm', query)
-    ])
-
-    console.log(result)
     try {
+        const result = await db.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', query)
+        ])
+
+        console.log(result)
         if (result.documents.length > 0) {
             const existingMovie = result.documents[0]
             await db.updateDocument(
@@ -29,11 +29,24 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
                 movie_id: movie.id,
                 count: 1,
                 poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-                // movie_title: movie.title,
+                movie_title: movie.title,
             })
         }
     } catch (error) {
         console.log(error)
         throw error
+    }
+}
+
+export const getTrendingmovies = async (): Promise<TrendingMovie[] | undefined> => {
+    try {
+        const result = await db.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count')
+        ])
+        return result.documents as unknown as TrendingMovie[]
+    } catch (error) {
+        console.log(error)
+        return undefined
     }
 }
